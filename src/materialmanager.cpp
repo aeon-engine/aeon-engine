7db7271eac3052c9bed950110a12438c701e71d8
Robin Degen <robindegen@gmail.com>
2014-10-25 16:21:55 +0200

    Fixed accidental invalid copied log messages

--- src/materialmanager.cpp
+++ src/materialmanager.cpp
@@ -1,3 +1,18 @@
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2014 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
 #include "stdafx.h"
 
 aeon_initialize_singleton(aeon::material_manager);
@@ -7,12 +22,12 @@ namespace aeon
 
 material_manager::material_manager()
 {
-    console::debug("[TextureManager]: created.");
+    console::debug("[MaterialManager]: created.");
 }
 
 material_manager::~material_manager()
 {
-    console::debug("[TextureManager]: disposed.");
+    console::debug("[MaterialManager]: disposed.");
 }
 
 material_ptr material_manager::load(stream_ptr stream)
