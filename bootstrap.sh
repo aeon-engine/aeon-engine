965187a49edd11d8c4095b6afcd1bd3289ae7eeb
Robin Degen <robindegen@gmail.com>
2015-06-01 18:29:34 +0200

    Updated dependency paths (temporary solution)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export BOOST_ROOT=/Development/Libraries/Binary/boost/current
-export GLFW_ROOT=/Development/Libraries/Binary/glfw/current
-export GLEW_ROOT=/Development/Libraries/Binary/glew/current
-export GLM_ROOT=/Development/Libraries/Binary/glm/current
-export ASSIMP_ROOT=/Development/Libraries/Binary/assimp/current
-export PNG_ROOT=/Development/Libraries/Binary/png/current
-export ZLIB_ROOT=/Development/Libraries/Binary/zlib/current
+export BOOST_ROOT=/Users/robindegen/Development/libraries/boost/current
+export GLFW_ROOT=/Users/robindegen/Development/libraries/glfw/3.1.1
+export GLEW_ROOT=/Users/robindegen/Development/libraries/glew/1.12.0
+export GLM_ROOT=/Users/robindegen/Development/libraries/glm/0.9.6.3
+export ASSIMP_ROOT=/Users/robindegen/Development/libraries/assimp/3.1.1
+export PNG_ROOT=/Users/robindegen/Development/libraries/png/1.6.17
+export ZLIB_ROOT=/Users/robindegen/Development/libraries/zlib/1.2.8
 rm -rf build
 mkdir build
 cd build
