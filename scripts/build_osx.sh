2596c9cdc4a640f0f57b3c27d64d3fc38b2c33d0
Robin Degen <robindegen@gmail.com>
2015-09-09 11:19:30 +0200

    Moved build script (gitlab-ci.yml) to seperate shell scripts so that it's more expandable and locally testable in the future.

---
+++ scripts/build_osx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git submodule update --init
+export WORKSPACE=$PWD
+cd $AEON_LIBRARIES_PATH
+git fetch --prune
+git checkout origin/master
+cd $WORKSPACE
+export GIT_COMMIT_HASH=`git rev-parse --short HEAD`
+export BUILD_DIR=ci_build
+mkdir $BUILD_DIR
+pushd $BUILD_DIR
+cmake -DCMAKE_INSTALL_PREFIX=install -DAEON_ENABLE_CLANG_COVERAGE=1 ../
+cmake --build . --config Debug -- -j4
+popd
