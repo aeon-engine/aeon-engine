4cfee60816a3c9494b77d0423ac9c03217d4b769
Robin Degen <robindegen@gmail.com>
2015-10-09 11:45:39 +0200

    Added ci build scripts for windows

---
+++ scripts/build_windows.cmd
@@ -0,0 +1,10 @@
+git submodule update --init
+pushd "%AEON_LIBRARIES_PATH%"
+git checkout origin/master
+git fetch --prune
+popd
+mkdir ci_build
+cd ci_build
+cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=install ../
+cmake --build . --config Debug
+
