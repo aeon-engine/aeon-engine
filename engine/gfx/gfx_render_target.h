f2a453836079f9504b61080987cee8d5f5e15799
Robin Degen <robindegen@gmail.com>
2015-10-19 13:25:19 +0200

    Added some documentation comments to gfx_render_target.h

--- engine/gfx/gfx_render_target.h
+++ engine/gfx/gfx_render_target.h
@@ -30,15 +30,53 @@ public:
     render_target();
     virtual ~render_target();
 
-    bool handle_frame(double dt);
-
+    /*!
+     * Add a frame listener to this render target. The on_frame event will
+     * be called each frame that this render target is rendering.
+     *
+     * Render target does not take ownership of this pointer, so it's up to
+     * the caller to keep this pointer alive, and delete it appropriately.
+     */
     void attach_frame_listener(frame_listener *listener);
+
+    /*!
+     * Detach a frame listener from this render target. This will not delete
+     * the frame listener, but merely detach it so events are no longer
+     * triggered.
+     */
     void detach_frame_listener(frame_listener *listener);
+
+    /*!
+     * Detach all frame listeners from this render target. This will not delete
+     * the frame listeners, but merely detach them so events are no longer
+     * triggered.
+     */
     void detach_all_frame_listeners();
 
+    /*!
+     * Internal function to handle the renderig of a frame. This is called by
+     * the platform interface that this render target is attached to.
+     */
+    bool handle_frame(double dt);
+
+    /*!
+     * Internal function to make this render target active. This is called by
+     * the platform interface that this render target is attached to.
+     *
+     * This function must be called before attempting to render anything, since
+     * the underlaying graphics driver needs to know that subsequent draw calls
+     * should be executed on this render target.
+     */
     virtual void make_current() = 0;
 
 protected:
+    /*!
+     * This function must be implemented by the platform/gfx driver specific
+     * implementation, and should do everything required to render to this render
+     * target.
+     *
+     * This function is called from handle_frame.
+     */
     virtual bool on_frame(double dt) = 0;
 
 private:
