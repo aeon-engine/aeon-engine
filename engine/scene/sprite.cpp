747274e6d41d2bee4c16b8f2f7bee276203c0c5c
Robin Degen <robindegen@gmail.com>
2016-01-25 09:51:02 +0100

    Added base sprite class (render object) for 2d rendering

---
+++ engine/scene/sprite.cpp
@@ -0,0 +1,53 @@
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2015 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#include <scene/sprite.h>
+
+namespace aeon
+{
+namespace scene
+{
+
+sprite::sprite(gfx::texture_ptr texture)
+    : size_(texture->get_size())
+{
+}
+
+sprite::sprite(gfx::texture_ptr texture, glm::vec2 size)
+    : size_(size)
+{
+}
+
+sprite::sprite(gfx::texture_ptr texture, float width, float height)
+    : size_(width, height)
+{
+}
+
+sprite::~sprite()
+{
+}
+
+void sprite::set_size(glm::vec2 size)
+{
+    size_ = size;
+}
+
+void sprite::set_size(float width, float height)
+{
+    size_ = glm::vec2(width, height);
+}
+
+} // namespace scene
+} // namespace aeon
---
+++ engine/scene/sprite.h
@@ -0,0 +1,47 @@
+/*
+ * ROBIN DEGEN; CONFIDENTIAL
+ *
+ * 2012 - 2015 Robin Degen
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of
+ * Robin Degen and its suppliers, if any. The intellectual and technical
+ * concepts contained herein are proprietary to Robin Degen and its suppliers
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this
+ * information or reproduction of this material is strictly forbidden unless
+ * prior written permission is obtained from Robin Degen.
+ */
+
+#pragma once
+
+#include <common/types/rectangle.h>
+#include <scene/render_object.h>
+#include <gfx/gfx_texture.h>
+#include <glm/vec2.hpp>
+#include <memory>
+
+namespace aeon
+{
+namespace scene
+{
+
+class sprite : public render_object
+{
+public:
+    explicit sprite(gfx::texture_ptr texture);
+    explicit sprite(gfx::texture_ptr texture, glm::vec2 size);
+    explicit sprite(gfx::texture_ptr texture, float width, float height);
+    virtual ~sprite();
+
+    void set_size(glm::vec2 size);
+    void set_size(float width, float height);
+
+private:
+    glm::vec2 size_;
+};
+
+using sprite_ptr = std::shared_ptr<sprite>;
+
+} // namespace scene
+} // namespace aeon
