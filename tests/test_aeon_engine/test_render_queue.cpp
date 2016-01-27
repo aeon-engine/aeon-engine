13ceb8e44f248922627a38d3620b7719879ee649
Robin Degen <robindegen@gmail.com>
2016-01-27 13:23:18 +0100

    Added unittest for sorting sprites in the render queue with group and z order

--- tests/test_aeon_engine/test_render_queue.cpp
+++ tests/test_aeon_engine/test_render_queue.cpp
@@ -49,3 +49,63 @@ TEST(test_render_queue, test_render_queue_sort_by_group)
         ++group_id;
     }
 }
+
+TEST(test_render_queue, test_render_queue_sort_by_group_and_sprite_z_order)
+{
+    scene::render_queue queue;
+
+    for (int i = 0; i <= 100; ++i)
+    {
+        int group_id = 100 - i;
+        for (int j = 0; j <= 100; ++j)
+        {
+            int zorder = 100 - j;
+
+            // We abuse width and height for checking the order later on.
+            scene::sprite_ptr s = std::make_shared<aeon::scene::sprite>(nullptr, glm::vec2(group_id, zorder), zorder);
+            queue.add_render_object(s, group_id);
+        }
+    }
+
+    int group_id = 100;
+    int zorder = 100;
+    for (auto obj : queue)
+    {
+        if (zorder == 100)
+            ASSERT_EQ(group_id, obj.first);
+
+        auto sprite = std::dynamic_pointer_cast<scene::sprite>(obj.second);
+        ASSERT_EQ(group_id, sprite->get_size().x);
+        ASSERT_EQ(zorder, sprite->get_size().y);
+
+        --zorder;
+
+        if (zorder < 0)
+        {
+            --group_id;
+            zorder = 100;
+        }
+    }
+
+    queue.sort();
+
+    group_id = 0;
+    zorder = 0;
+    for (auto obj : queue)
+    {
+        if (zorder == 0)
+            ASSERT_EQ(group_id, obj.first);
+
+        auto sprite = std::dynamic_pointer_cast<scene::sprite>(obj.second);
+        ASSERT_EQ(group_id, sprite->get_size().x);
+        ASSERT_EQ(zorder, sprite->get_size().y);
+
+        ++zorder;
+
+        if (zorder > 100)
+        {
+            ++group_id;
+            zorder = 0;
+        }
+    }
+}
