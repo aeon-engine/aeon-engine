5db30b4d22d3629425d3a7a2b02f7fb35a6f48e6
Robin Degen <robindegen@gmail.com>
2016-01-27 13:58:26 +0100

    Enabled multicore building on windows to lower compile time

--- scripts/build_windows.cmd
+++ scripts/build_windows.cmd
@@ -6,5 +6,5 @@ popd
 mkdir ci_build
 cd ci_build
 cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=install ../
-cmake --build . --config Debug
+cmake --build . --config Debug -- /m
 ctest -C Debug --verbose
