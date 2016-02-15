18dacd39ff9503fc53cdf925609750d6abdd03cc
Robin Degen <robindegen@gmail.com>
2016-02-15 08:13:08 +0100

    Enabled double buffering, disabled window resizing and made sure a newly created window gets focus.

--- engine/platform/glfw/platform_glfw_window.cpp
+++ engine/platform/glfw/platform_glfw_window.cpp
@@ -33,6 +33,9 @@ platform_window::platform_window(platform_interface *interface, int width, int h
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_FOCUSED, GL_TRUE);
 
     window_ = glfwCreateWindow(width, height, title.c_str(), monitor, nullptr);
 
