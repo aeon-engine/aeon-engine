cfa08c2b1c482239c9c81d868457b4a400b52dcd
Robin Degen <robindegen@gmail.com>
2016-02-16 10:45:57 +0100

    Cleaned up buildscripts We no longer use an external libraries repository. AEON_ENABLE_CLANG_COVERAGE currently doesn't work.

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 git submodule update --init
-export WORKSPACE=$PWD
-cd $AEON_LIBRARIES_PATH
-git fetch --prune
-git checkout origin/master
-cd $WORKSPACE
 export GIT_COMMIT_HASH=`git rev-parse --short HEAD`
 export BUILD_DIR=ci_build
 mkdir $BUILD_DIR
 cd $BUILD_DIR
-cmake -DCMAKE_INSTALL_PREFIX=install -DAEON_ENABLE_CLANG_COVERAGE=1 ../
+cmake -DCMAKE_INSTALL_PREFIX=install ../
 cmake --build . --config Debug -- -j4
 ctest -C Debug --verbose
