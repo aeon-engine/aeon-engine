3dda81d92de015c98c9c6e9268e9a9fd2bb295b7
Robin Degen <robindegen@gmail.com>
2016-03-13 13:56:22 +0100

    Fixed windows build reporting green when something actually fails.

--- scripts/build_windows.cmd
+++ scripts/build_windows.cmd
@@ -1,6 +1,36 @@
 git submodule update --init
+
+if errorlevel 1 (
+    exit /b 1
+)
+
 mkdir ci_build
+
+if errorlevel 1 (
+    exit /b 1
+)
+
 cd ci_build
+
+if errorlevel 1 (
+    exit /b 1
+)
+
 cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=install ../
+
+if errorlevel 1 (
+    exit /b 1
+)
+
 cmake --build . --config Debug -- /m
+
+if errorlevel 1 (
+    exit /b 1
+)
+
 ctest -C Debug --verbose
+
+if errorlevel 1 (
+    exit /b 1
+)
+
