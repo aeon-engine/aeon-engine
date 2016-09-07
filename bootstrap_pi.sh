9f072b324aeec53c53dbfe6edfe838c9e754e0f6
Robin Degen <robindegen@gmail.com>
2016-09-07 09:26:57 +0200

    Fixed bootstrap_pi.sh to actually work on a raspberry pi 3.

--- bootstrap_pi.sh
+++ bootstrap_pi.sh
@@ -1,11 +1,9 @@
-
 #!/bin/bash
 
-export CC=${RASPBERRY_PI_GCC_PATH}/bin/arm-unknown-linux-gnueabi-gcc
-export CXX=${RASPBERRY_PI_GCC_PATH}/bin/arm-unknown-linux-gnueabi-g++
+export RASPBERRY_PI_SDK_ROOT=/opt/vc
 
 rm -rf build
 mkdir build
 cd build
-cmake ../ -DAEON_GFX_RENDERER=GLES2 -DAEON_PLATFORM=RaspberryPi -DCMAKE_FIND_ROOT_PATH=${RASPBERRY_PI_GCC_PATH}/arm-unknown-linux-gnueabi -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_SYSTEM_VERSION=1
+cmake ../ -DAEON_GFX_RENDERER=GLES2 -DAEON_PLATFORM=RaspberryPi -DAEON_SKIP_COPY_ASSETS_TO_RUNTIME_PATH=1
 cd ..
