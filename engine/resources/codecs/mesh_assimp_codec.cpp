55d6548dcf711aee2ccf21feaa1414f96cfffc3b
Robin Degen <robindegen@gmail.com>
2016-09-13 16:58:18 +0200

    Fixed matrix conversion in the assimp mesh codec resulting in an incorrect matrix being returned.

--- engine/resources/codecs/mesh_assimp_codec.cpp
+++ engine/resources/codecs/mesh_assimp_codec.cpp
@@ -215,10 +215,10 @@ glm::vec3 mesh_codec_assimp::__convert_to_glm_vec3(const aiVector3D &vector) con
 glm::mat4 mesh_codec_assimp::__convert_to_glm_mat4(const aiMatrix4x4 &matrix) const
 {
     return glm::mat4(
-        matrix.a1, matrix.a2, matrix.a3, matrix.a4,
-        matrix.b1, matrix.b2, matrix.b3, matrix.b4,
-        matrix.c1, matrix.c2, matrix.c3, matrix.c4,
-        matrix.d1, matrix.d2, matrix.d3, matrix.d4);
+        matrix.a1, matrix.b1, matrix.c1, matrix.d1,
+        matrix.a2, matrix.b2, matrix.c2, matrix.d2,
+        matrix.a3, matrix.b3, matrix.c3, matrix.d3,
+        matrix.a4, matrix.b4, matrix.c4, matrix.d4);
 }
 
 common::types::color mesh_codec_assimp::__convert_to_color(const aiColor4D &color) const
