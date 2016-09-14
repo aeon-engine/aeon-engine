36d816df308dcb21aee0495481ab19cd594b5472
Robin Degen <robindegen@gmail.com>
2016-09-14 12:54:16 +0200

    Added clang format config file and script to run it recursively on the engine.

---
+++ clang_format.sh
@@ -0,0 +1,10 @@
+CLANG_FORMAT="clang-format"
+
+function format {
+    DIRECTORY=$1
+    echo "Formatting code under $DIRECTORY/"
+    find "$DIRECTORY" \( -name '*.h' -or -name '*.cpp' \) -print0 | xargs -0 "$CLANG_FORMAT" -i
+}
+
+format $(PWD)/engine
+format $(PWD)/examples
