9f3ad36a47aa9a4194dfdf29d1ccdb8d4cc2b4ef
Robin Degen <robindegen@gmail.com>
2016-09-28 14:43:58 +0200

    Splitters!

--- editor/application/frm_mainwindow_view.cpp
+++ editor/application/frm_mainwindow_view.cpp
@@ -15,6 +15,8 @@
 
 #include <frm_mainwindow_view.h>
 #include <ui_frm_mainwindow.h>
+#include <QSplitter>
+#include <QOpenGLWidget>
 
 namespace aeon
 {
@@ -26,6 +28,20 @@ frm_mainwindow_view::frm_mainwindow_view()
     , ui_(new Ui::MainWindow)
 {
     ui_->setupUi(this);
+
+    QSplitter *vsplitter = new QSplitter(Qt::Vertical, this);
+    QSplitter *hsplitter1 = new QSplitter(Qt::Horizontal, vsplitter);
+    vsplitter->addWidget(hsplitter1);
+    QSplitter *hsplitter2 = new QSplitter(Qt::Horizontal, vsplitter);
+    vsplitter->addWidget(hsplitter2);
+
+    hsplitter1->addWidget(new QOpenGLWidget(hsplitter1));
+    hsplitter1->addWidget(new QOpenGLWidget(hsplitter1));
+
+    hsplitter2->addWidget(new QOpenGLWidget(hsplitter2));
+    hsplitter2->addWidget(new QOpenGLWidget(hsplitter2));
+
+    setCentralWidget(vsplitter);
 }
 
 frm_mainwindow_view::~frm_mainwindow_view()
