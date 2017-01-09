21741574d90d17bfe9da3d388d7f83e4c4e61d6a
Robin Degen <robindegen@gmail.com>
2017-01-09 14:04:30 +0100

    Experimental: Re-enable linux build node.

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+# Cleanup
+git clean -dfx
+git submodule foreach git clean -dfx
+git reset --hard
+git submodule foreach git reset --hard
+
+# Update
 git submodule update --init
+
+# Build
 export GIT_COMMIT_HASH=`git rev-parse --short HEAD`
 export BUILD_DIR=ci_build
+rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 cmake -DCMAKE_INSTALL_PREFIX=install ../
-cmake --build . --config Debug -- -j4
+cmake --build . --config Debug -- -j2
 ctest -C Debug --verbose
